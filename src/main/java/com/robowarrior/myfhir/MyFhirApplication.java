f883ffe4ccbcfbdf75ee0340a4dfd57b2507a67a
Tanmay Darmorha <tanmaydarmorha@gmail.com>
2022-01-22 00:53:18 +0530

    initial commit

---
+++ src/main/java/com/robowarrior/myfhir/MyFhirApplication.java
@@ -0,0 +1,13 @@
+package com.robowarrior.myfhir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFhirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFhirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robowarrior/myfhir/MyfhirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robowarrior.myfhir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfhirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
